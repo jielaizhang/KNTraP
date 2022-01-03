933991bab7e7668fb1d5f55b3d8eac289922158b
Jielai Zhang <zhang.jielai@gmail.com>
2022-01-04 10:17:53 +1100

    Used.

---
+++ scripts/stack_ccdbyccd.sh
@@ -0,0 +1,22 @@
+for ext in {0..61}
+do
+      echo ###################
+      echo --------
+      echo "swarp CPinstcal_GRB210605A5*_20210607*_g_*/*ext$ext.fits* -IMAGEOUT_NAME GRB210605A5_20210607/GRB210605A5_20210607_g_stack_ext$ext.fits"
+      swarp CPinstcal_GRB210605A5*_20210607*_g_*/*ext$ext.fits* -IMAGEOUT_NAME GRB210605A5_20210607/GRB210605A5_20210607_g_stack_ext$ext.fits
+      echo --------
+      echo "swarp CPinstcal_GRB210605A5*_20210607*_i_*/*ext$ext.fits* -IMAGEOUT_NAME GRB210605A5_20210607/GRB210605A5_20210607_i_stack_ext$ext.fits"
+      swarp CPinstcal_GRB210605A5*_20210607*_i_*/*ext$ext.fits* -IMAGEOUT_NAME GRB210605A5_20210607/GRB210605A5_20210607_i_stack_ext$ext.fits
+      echo --------
+      echo "swarp CPinstcal_GRB210605A5*_20210608*_g_*/*ext$ext.fits* -IMAGEOUT_NAME GRB210605A5_20210608/GRB210605A5_20210608_g_stack_ext$ext.fits"
+      swarp CPinstcal_GRB210605A5*_20210608*_g_*/*ext$ext.fits* -IMAGEOUT_NAME GRB210605A5_20210608/GRB210605A5_20210608_g_stack_ext$ext.fits
+      echo --------
+      echo "swarp CPinstcal_GRB210605A5*_20210608*_i_*/*ext$ext.fits* -IMAGEOUT_NAME GRB210605A5_20210608/GRB210605A5_20210608_i_stack_ext$ext.fits"
+      swarp CPinstcal_GRB210605A5*_20210608*_i_*/*ext$ext.fits* -IMAGEOUT_NAME GRB210605A5_20210608/GRB210605A5_20210608_i_stack_ext$ext.fits
+      echo --------
+      echo "swarp CPinstcal_GRB210605A5*_20210609*_g_*/*ext$ext.fits* -IMAGEOUT_NAME GRB210605A5_20210609/GRB210605A5_20210609_g_stack_ext$ext.fits"
+      swarp CPinstcal_GRB210605A5*_20210609*_g_*/*ext$ext.fits* -IMAGEOUT_NAME GRB210605A5_20210609/GRB210605A5_20210609_g_stack_ext$ext.fits
+      echo --------
+      echo "swarp CPinstcal_GRB210605A5*_20210609*_i_*/*ext$ext.fits* -IMAGEOUT_NAME GRB210605A5_20210609/GRB210605A5_20210609_i_stack_ext$ext.fits"
+      swarp CPinstcal_GRB210605A5*_20210609*_i_*/*ext$ext.fits* -IMAGEOUT_NAME GRB210605A5_20210609/GRB210605A5_20210609_i_stack_ext$ext.fits
+done
