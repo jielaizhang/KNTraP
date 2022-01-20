0a5865a6bd6d1a5485ac5f6aea1e0f31d0d23c16
Jielai Zhang <zhang.jielai@gmail.com>
2022-01-20 16:53:27 +1100

    ready to test

---
+++ template_kntrappipe_loop.sh
@@ -0,0 +1,13 @@
+for fieldname in GRB210605A5 GRB210605A4
+do
+    for caldate in 20210607 20210608
+    do
+        for band in g
+        do
+            for ext in {31..33}
+            do
+                echo #############
+                echo -------------
+                echo python submit_slurm_ozstar.py -v $fieldname $caldate $band $ext
+                python submit_slurm_ozstar.py -v $fieldname $caldate $band $ext
+
