263db34eb037e38feb233d97c1963597c67c4b9f
Jielai Zhang <zhang.jielai@gmail.com>
2022-01-21 12:26:13 +1100

    update

--- notes/submit_slurm_OzSTAR_batch.py
+++ notes/submit_slurm_OzSTAR_batch.py
@@ -3,7 +3,7 @@
 """ submit_slurm_OzSTAR_batch.py -- Input file with commands to submit to sbatch, and submit each command.  
 
 Usage: 
-    submit_slurm_OzSTAR_batch [-h] [-v] [--do_not_submit] [--ozstar_reservation STRING] [--bashrcfile STRING] [--skiplog] [--request_memory INT] <commandfile> 
+    submit_slurm_OzSTAR_batch [-h] [-v] [--debug] [--do_not_submit] [--ozstar_reservation STRING] [--bashrcfile STRING] [--skiplog] [--request_memory INT] <commandfile> 
 
 Arguments:
     commandfile (string)
@@ -12,6 +12,7 @@ Arguments:
 Options:
     -h, --help                      Show this screen
     -v, --verbose                   Print extra info to screen. [default: False]
+    --debug                         Print input docopt arguments [default: False]
     --do_not_submit                 Just write the slurm script and pipeline bash scripts, don't submit via sbatch [default: False]
     --bashrcfile STRING             to set up env [default: /fred/oz100/NOAO_archive/KNTraP_Project/src/photpipe/config/DECAMNOAO/YSE/YSE.bash.sourceme]
     --ozstar_reservation STRING     If set, in sbatch script put #SBATCH --reservation={ozstar_reservation}
@@ -23,7 +24,7 @@ Examples:
 """
 import docopt
 import sys, os
-import numpy
+import numpy as np
 
 __author__      = "Jielai Zhang"
 __license__     = "MIT"
@@ -73,59 +74,71 @@ duration=$SECONDS
 echo Slurm Job JOB_NAME done in $(($duration / 60)) minutes and $(($duration % 60)) seconds
 '''
 
-def submit_slurm_OzSTAR_batch(command,
+def submit_slurm_OzSTAR_batch(commandfile,
                                 bashrcfile='/fred/oz100/NOAO_archive/KNTraP_Project/src/photpipe/config/DECAMNOAO/YSE/YSE.bash.sourceme',
                                 ozstar_reservation=None,
                                 memory_request=4000,
-                                verbose=False,debugmode=False,quietmode=False,
+                                verbose=False,
                                 do_not_submit=False):
-    # Define slurm job name 
-    slurm_job_name      = '_'.join(command.split(' '))
-    fieldname           = command.split(' ')[2]
-
-    # Figure out where to save the slurm script
+    # Get environment variables for pipeline set up
     pipedata_dir = os.getenv('PIPE_DATA')
-    slurm_script_dir = pipedata_dir+f'/logs/ozstar/{fieldname}'
-    slurm_script_path    = slurm_script_dir+f'/{slurm_job_name}_slurm.sh'
 
-    # Create output directory if not exist
-    just_created  = create_dir_ifnot(slurm_script_dir)
-    if just_created == True:
-        if verbose == True or debugmode == True:
-            print(f'VERBOSE/DEBUG: {slurm_script_dir} was just created.')
+    with open(commandfile) as fp:
+        pipecommand = fp.readline().strip()
+        cnt = 1
+        while pipecommand:
+            print('==========')
+            print(f"Line {cnt}: {pipecommand}")
 
-    # Create slurm batch bash script
-    script_string = batch_script_template.replace('JOB_NAME',slurm_job_name)
-    script_string = script_string.replace('PIPE_DATA_DIR',pipedata_dir)
-    script_string = script_string.replace('COMMAND',command)
-    script_string = script_string.replace('FIELDNAME',fieldname)
-    script_string = script_string.replace('MEM_REQUEST',int(np.ceil(memory_request/1000.)) )
-    if ozstar_reservation == None:
-        script_string = script_string.replace('RESERVATION_LINE','')
-    else:
-        script_string = script_string.replace('RESERVATION_LINE',f'#SBATCH --reservation={ozstar_reservation}')
-    
-    
-    # Write the bash script to file
-    f = open(slurm_script_path,'w')
-    f.write(script_string)
-    f.close()
+            # Define slurm job name 
+            slurm_job_name      = '_'.join(pipecommand.split(' '))
+            fieldname           = pipecommand.split(' ')[2]
 
-    # print if not quietmode
-    if not quietmode:
-        print(f'Saved: {slurm_script_path}')
+            # Figure out where to save the slurm script
+            slurm_script_dir    = pipedata_dir+f'/logs/ozstar/{fieldname}'
+            slurm_script_path   = slurm_script_dir+f'/{slurm_job_name}_slurm.sh'
 
-    # submit slurm script
-    if do_not_submit == False:
-        command = f'sbatch {slurm_script_path}'
-        print(f'Running: {command}')
-        try:
-            os.system(command)
-        except:
-            sys.exit(f'!!! ERROR-- sys.exit when running: {command}')
+            # Create output directory if not exist
+            just_created  = create_dir_ifnot(slurm_script_dir)
+            if just_created == True:
+                if verbose == True:
+                    print(f'VERBOSE/DEBUG: {slurm_script_dir} was just created.')
+
+            # Create slurm batch bash script
+            script_string = batch_script_template.replace('JOB_NAME',slurm_job_name)
+            script_string = script_string.replace('PIPE_DATA_DIR',pipedata_dir)
+            script_string = script_string.replace('COMMAND',pipecommand)
+            script_string = script_string.replace('FIELDNAME',fieldname)
+            script_string = script_string.replace('MEM_REQUEST',str(int(np.ceil(memory_request/1000.))) )
+            if ozstar_reservation == None:
+                script_string = script_string.replace('RESERVATION_LINE','')
+            else:
+                script_string = script_string.replace('RESERVATION_LINE',f'#SBATCH --reservation={ozstar_reservation}')
+            
+            
+            # Write the bash script to file
+            f = open(slurm_script_path,'w')
+            f.write(script_string)
+            f.close()
+
+            # print 
+            print(f'Saved: {slurm_script_path}')
+
+            # submit slurm script
+            if do_not_submit == False:
+                sbatchcommand = f'sbatch {slurm_script_path}'
+                print(f'Running: {sbatchcommand}')
+                try:
+                    os.system(sbatchcommand)
+                except:
+                    sys.exit(f'!!! ERROR-- sys.exit when running: {command}')
+
+            # read in next line
+            pipecommand = fp.readline().strip()
+            cnt += 1
             
     # Finish
-    return slurm_script_path
+    return None
 
 
 ############################################################################
@@ -141,22 +154,18 @@ if __name__ == "__main__":
     if debugmode:
         print(arguments)
     verbose             = arguments['--verbose']
-    quietmode           = arguments['--quietmode']
     do_not_submit       = arguments['--do_not_submit']
     # Required arguments
-    command             = arguments['<command>']
-    filterband          = arguments['<filterband>']
-    ctio_caldate        = arguments['<ctio_caldate>']
-    fitsextension       = arguments['<fitsextension>']
+    commandfile         = arguments['<commandfile>']
     # Optional arguments (with defaults set)
     bashrcfile          = arguments['--bashrcfile']
     ozstar_reservation  = arguments['--ozstar_reservation']
-    memory_request      = arguments['--request_memory']
+    memory_request      = int(arguments['--request_memory'])
     _                   = arguments['--skiplog']
 
-    _ = submit_slurm_OzSTAR_batch(command,
+    _ = submit_slurm_OzSTAR_batch(commandfile,
                                 bashrcfile=bashrcfile,
                                 ozstar_reservation=ozstar_reservation,
                                 memory_request = memory_request,
-                                verbose=verbose,debugmode=debugmode,quietmode=quietmode,
+                                verbose=verbose,
                                 do_not_submit=do_not_submit)
